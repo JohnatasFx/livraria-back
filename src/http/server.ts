1c9033c8a6566880246a8297a5ea2423040e31c1
Johnatas Félix <johnatasfelix04@gmail.com>
2024-02-23 21:32:50 -0300

    deploy: alterado banco de dados da aplicação

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,16 +1,18 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
-import { createBook, updateBook, getBook, getAllBooks, deleteBook} from '../controllers/book';
+import { createBook, updateBook, getBook, getAllBooks, deleteBook } from '../controllers/book';
 import cors from "cors";
 
 const app = express();
+
 const port = process.env.PORT || 3000;
 
-// app.get("/", (req: Request, res: Response) => {
-//     res.send("Hello World!");
-// });
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).send("Server is running!");
+});
 
 app.use(cors());
+
 app.use(bodyParser.json());
 
 app.post("/book", createBook);
